bd631dc261ccd68143afb28e70b2004a5e1a6946
sumiliholden <sumiliholden@gmail.com>
2022-02-07 07:36:20 +0800

    imported tailwind css

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,6 @@
 import 'tailwindcss/tailwind.css'
-
+import "@fortawesome/fontawesome-free/css/all.min.css";
+import "/public/dist/assets/styles/tailwind.css";
 const App = ({ Component, pageProps }) => <Component {...pageProps} />
 
 export default App
